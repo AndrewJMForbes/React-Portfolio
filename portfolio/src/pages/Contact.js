d2a54fdbb16ec7d18e45ebc3e1eda19b2c9e2e1f
Forbes85 <vudo8585@gmail.com>
2023-08-24 22:01:43 -0500

    started resume

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,51 +1,12 @@
-import { useState } from "react";
+import ContactForm from "../components/form.js"
 function Contact() {
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
-const [textarea, setTextarea] = useState("");
 
-const handleFormSubmit = (e) => {
-  e.preventDefault();
-  alert(`Thank you for your submission ${name}!
-  Your email ${email} has been added! Thank you for saying ${textarea}`)
-  setName("");
-};
-
-const handleInputChange = (e) => {
-  const { name, value } = e.target;
-  
-  return name === "name" ? setName(value) : email === "email" ?
-  setEmail(value) : textarea === "textarea" ? setTextarea(value) : setTextarea(value) 
-
-};
 
   return (
     <div>
       <h1 className="page-title">Contact</h1>
       <div className="contactContainer">
-     <form>
-      <label className="name">Name</label>
-      <input 
-      type="text" 
-      value={name}
-      onChange={handleInputChange}
-      placeholder="Enter your name"/>
-      <label className="email">Email</label>
-      <input 
-      type="email" 
-      value={email}
-      onChange={handleInputChange}
-      placeholder="Enter your email"/>
-      <label className="message">Message</label>
-      <textarea 
-      type="text"
-      value={textarea} 
-      onChange={handleInputChange}
-      placeholder="Enter your message here"
-      ></textarea>
-     <input type="submit" onClick={handleFormSubmit} className="submit"/>
-
-     </form>
+      <ContactForm />
       </div>
     </div>
   );
--- portfolio/src/pages/Resume.js
+++ portfolio/src/pages/Resume.js
@@ -1,7 +1,49 @@
+import github from "../images/github.png";
+
+
 function Resume() {
   return (
-    <div className="resumeContainer">
+    <div>
       <h1 className="page-title">Resume</h1>
+      <div className="resumeContainer">
+        <div className="basicInfo">
+          <h1 className="resumeName">Andrew Forbes</h1>
+          <h3>Full Stack Web Developer</h3>
+          <a href="mailto:vudo8585@gmail.com"><h5 >✉️vudo8585@gmail.com</h5></a>
+          <h5>📞(217)480-0408</h5>
+          <h5><span className="location">📍</span>Champaign, IL</h5>
+          <a href="mailto:vudo8585@gmail.com"><h5><img className="resumeContact" src={github} alt="something" />GitHub</h5></a>
+          </div>
+          <div className="resume">
+          <h2>Technical Skills</h2>
+          <h3>Front End</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Handlebars</li>
+            <li>Materialize</li>
+            <li>Responsive Design</li>
+            <li>Mobile-First Design</li>
+            </ul>  
+            <h3>Back End</h3>
+            <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST APIs</li>
+            <li>GraphQL</li>
+            <li>JSON</li>
+            <li>Web APIs</li>
+            <li>Authentication</li>
+            <li>Progressive Web Applications</li>
+            </ul>
+          </div>
+      </div>
     </div>
   )
 }
