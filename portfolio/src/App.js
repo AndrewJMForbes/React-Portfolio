c5faeae5d6fc5c8547455c22ff15c65caaea276d
Forbes85 <vudo8585@gmail.com>
2023-08-26 18:16:07 -0500

    deployment 1

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Reference from "./pages/Reference";
 import Projects from "./pages/Projects";
@@ -43,7 +44,7 @@ function App() {
     </Router>
 
     </div>
-  
+    <Footer />
     </div>
   );
 }
